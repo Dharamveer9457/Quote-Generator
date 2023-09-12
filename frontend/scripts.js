7f5771b0a9691f82044c013f26112608f4d30b34
Dharamveer <dharammveer945734@outlook.com>
2023-09-12 21:13:30 +0530

    Frontend part bugs fixed

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const generateButton = document.getElementById('generate-button');
     const themeInput = document.getElementById('theme-input');
     const quoteElement = document.getElementById('quote');
+    const loader = document.getElementById('loader');
 
     generateButton.addEventListener('click', generateQuote);
 
@@ -15,14 +16,27 @@ document.addEventListener('DOMContentLoaded', function () {
                 confirmButtonText: 'OK'
               });
         } else {
+            loader.style.display = 'block'; // Show loader
+            quoteElement.style.display = 'none';
+
             fetch(`https://quote-generator-lhbc.onrender.com/get-quote/${theme}`)
             .then((response) => response.json())
             .then((data) => {
-                // Append the generated quote to the quote container
+                loader.style.display = 'none';
+                quoteElement.style.display = 'block';
                 quoteElement.textContent = data.quote;
             })
             .catch((error) => {
                 console.error("Error fetching quote:", error);
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: 'Failed to fetch a quote. Please try again later.',
+                    confirmButtonText: 'OK'
+                });
+                loader.style.display = 'none';
+                quoteElement.style.display = 'block';
+                quoteElement.textContent = 'Type your theme and click on Generate Quote';
             });
         }
     }
